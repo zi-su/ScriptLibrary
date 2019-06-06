6cc8f8487d3d5802ac476e5595e06dc7f8ec42a9
Masato Naito <aitona765@gmail.com>
2019-06-06 20:22:56 +0900

    no message

---
+++ CustomEventSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class CustomEventSystem : SingletonMonobehaviour<CustomEventSystem>
+{
+    EventSystem _current;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        _current = EventSystem.current;
+        if(_current == null)
+        {
+            _current = gameObject.AddComponent<EventSystem>();
+        }
+    }
+
+    public EventSystem Current
+    {
+        get { return _current; }
+    }
+}
