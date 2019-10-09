2200eb4963c45347cae7b85b0b9b1078ff26e87b
Masato Naito <aitona765@gmail.com>
2019-10-09 23:22:43 +0900

    no message

--- Editor/AASUtility.cs
+++ Editor/AASUtility.cs
@@ -41,20 +41,42 @@ public class AASUtility : UnityEditor.Editor
         var g = CreateGroup(groupName);
         s.CreateOrMoveEntry(assetGuid, g);
     }
-
+    static void SetLabelToAsset(List<string> assetGuidList, string label, bool flag)
+    {
+        var s = GetSettings();
+        //ラベルを追加するように呼んでおく。追加されていないと設定されない。
+        s.AddLabel(label);
+        List<UnityEditor.AddressableAssets.Settings.AddressableAssetEntry> assetList = new List<UnityEditor.AddressableAssets.Settings.AddressableAssetEntry>();
+        s.GetAllAssets(assetList, true);
+        foreach (var assetGuid in assetGuidList)
+        {
+            var asset = assetList.Find((a) => { return a.guid == assetGuid; });
+            if(asset != null)
+            {
+                asset.SetLabel(label, flag);
+            }
+        }
+    }
     static void RemoveAssetFromGroup(string assetGuid)
     {
         var s = GetSettings();
         s.RemoveAssetEntry(assetGuid);
     }
 
+    static void BuildPlayerContent()
+    {
+        var d = GetSettings();
+        UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.BuildPlayerContent();
+    }
     [UnityEditor.MenuItem("test/run")]
     static public void Test()
     {
         var d = GetSettings();
-        UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.BuildPlayerContent();
-        //var matguid = UnityEditor.AssetDatabase.AssetPathToGUID("Assets/Data/hogeMat.mat");
-        //RemoveAssetFromGroup(matguid);
-        //AddAssetToGroup(matguid, "Prefab");
+
+        var matguid = UnityEditor.AssetDatabase.AssetPathToGUID("Assets/Data/hogeMat.mat");
+        AddAssetToGroup(matguid, "CreatedGroup");
+        ////List<string> assetGuidList = new List<string>() { matguid };
+        ////SetLabelToAsset(assetGuidList, "mat", true);
+        //CreateGroup("CreatedGroup");
     }
 }
