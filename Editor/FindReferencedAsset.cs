b4a2de8e4ab3651fe195eea17fe5f7ff15e74d7d
Masato Naito <aitona765@gmail.com>
2019-10-11 08:17:15 +0900

    対象アセットを参照しているアセットを検索

---
+++ Editor/FindReferencedAsset.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+public class FindReferencedAsset : Editor
+{
+    [MenuItem("test/find")]
+    static void Find()
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        var guids = Selection.assetGUIDs;
+        foreach (var guid in guids)
+        {
+            var targetPath = AssetDatabase.GUIDToAssetPath(guid);
+            var allassetpath = AssetDatabase.GetAllAssetPaths();
+            foreach (var a in allassetpath)
+            {
+                var dependencies = AssetDatabase.GetDependencies(a);
+                foreach (var d in dependencies)
+                {
+                    if(string.Equals(targetPath, d))
+                    {
+                        Debug.Log(a);
+                    }
+                }
+            }
+        }
+        sw.Stop();
+        Debug.Log(sw.ElapsedMilliseconds);
+    }
+}
