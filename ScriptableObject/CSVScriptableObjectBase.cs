addbaa67f6a3a85d8aa5ee400329b609209868b3
Masato Naito <aitona765@gmail.com>
2019-10-27 22:37:16 +0900

    no message

---
+++ ScriptableObject/CSVScriptableObjectBase.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVScriptableObjectBase : ScriptableObject
+{
+
+    /// <summary>
+    /// CSV全行を受け取りパースしてデータを追加
+    /// </summary>
+    /// <param name="lines"></param>
+    public virtual void Convert(string[] lines)
+    {
+
+    }
+}
