336d1e101a914f1818c0483770f73a027a6a4860
Masato Naito <aitona765@gmail.com>
2019-11-03 22:48:01 +0900

    no message

---
+++ Collision/HitAreaBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class HitAreaBase : MonoBehaviour
+{
+    public UnityAction enterAction;
+    public UnityAction stayAction;
+    public UnityAction exitAction;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+        enterAction?.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Stay");
+        stayAction?.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Leave");
+        exitAction?.Invoke();
+    }
+}
