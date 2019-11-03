7d0f9f1ea8ca8889347cebea79049d9223c6c2ec
Masato Naito <aitona765@gmail.com>
2019-11-04 02:28:03 +0900

    no message

--- UI/DialogYesNo.cs
+++ UI/DialogYesNo.cs
@@ -2,37 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-public class Dialog : MonoBehaviour
+public class DialogYesNo : MonoBehaviour
 {
     InputManager inputManager;
     InputPad inputPad;
 
     [SerializeField]
-    ButtonBase button;
+    ButtonBase[] buttons;
     int index;
 
-    public void SetAction(UnityAction submitAction)
+    public void SetAction(UnityAction cancelAction, UnityAction submitAction)
     {
-        button.OnClick += submitAction;
+        buttons[0].OnClick += cancelAction;
+        buttons[1].OnClick += submitAction;
     }
     // Start is called before the first frame update
     void Start()
     {
         inputManager = InputManager.Instance();
         inputPad = inputManager.CreateInputPad();
-        CustomEventSystem.Instance().Enter(button.gameObject);
+        CustomEventSystem.Instance().Enter(buttons[0].gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (inputPad.IsTrigger(InputType.Button.Right))
+        if (inputPad.IsTrigger(InputType.Button.KeyLeft) || inputPad.IsTrigger(InputType.Button.KeyRight))
+        {
+            index++;
+            if(index > buttons.Length - 1) { index = 0; }
+            CustomEventSystem.Instance().Enter(buttons[index].gameObject);
+        }
+        else if (inputPad.IsTrigger(InputType.Button.Right))
         {
             CustomEventSystem.Instance().Click();
         }
         else if (inputPad.IsTrigger(InputType.Button.Down))
         {
-            button.OnClick?.Invoke();
+            buttons[0].OnClick?.Invoke();
             Destroy(gameObject);
         }
     }
