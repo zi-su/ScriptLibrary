d04720949bc8e1a5aa4e9c88629137a158ea3524
Masato Naito <aitona765@gmail.com>
2019-11-05 00:02:49 +0900

    no message

---
+++ Manager/SceneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+public class SceneManager : Singleton<SceneManager>
+{
+    UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle handle;
+    bool isLoading;
+
+    public bool IsLoading()
+    {
+        return isLoading;
+    }
+    public void LoadScene(object key)
+    {
+        isLoading = true;
+        if (handle.IsValid())
+        {
+            Addressables.Release(handle);
+        }
+        Fade.Instance().Play(Fade.Mode.Out, Color.black, new Color(0.0f, 0.0f, 0.0f, 0.0f), action:()=> {
+            handle = Addressables.LoadSceneAsync(key);
+            handle.Completed += Handle_Completed;
+        });
+    }
+
+    private void Handle_Completed(UnityEngine.ResourceManagement.AsyncOperations.AsyncOperationHandle obj)
+    {
+        isLoading = false;
+        Fade.Instance().Play(Fade.Mode.In, new Color(0.0f, 0.0f, 0.0f, 0.0f));
+        Resources.UnloadUnusedAssets();
+    }
+}
